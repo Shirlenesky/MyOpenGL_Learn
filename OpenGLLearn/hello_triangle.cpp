4a3b0665e6d16e94fa1fa506a4473308b1d09316
shirlenesky <2411038489@qq.com>
2022-07-17 07:34:02 +0800

    第一章结束

--- OpenGLLearn/hello_triangle.cpp
+++ OpenGLLearn/hello_triangle.cpp
@@ -21,7 +21,6 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
-//"layout (location = 0) int vec3 aPos;\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
